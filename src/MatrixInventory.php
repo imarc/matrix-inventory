ae8e064a826f76b08b37612650b1a24eb338ea26
Linnea <linneahartsuyker@imarc.com>
2022-12-08 13:39:41 -0500

    PHP 8 update

--- src/MatrixInventory.php
+++ src/MatrixInventory.php
@@ -206,7 +206,7 @@ class MatrixInventory extends Plugin
      *
      * @return \craft\base\Model|null
      */
-    protected function createSettingsModel()
+    protected function createSettingsModel(): ?Model
     {
         return new Settings();
     }
